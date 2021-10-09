6bea78f78b857895d89a39314180f7914cd31922
Caleb1991 <calebjwittman1991@gmail.com>
2021-10-09 16:39:28 -0600

    Create snack model

--- spec/models/snack_spec.rb
+++ spec/models/snack_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Snack do
   describe 'validations' do
-    it {should validate_presence_of(name)}
-    it {should validate_presence_of(description)}
-    it {should validate_presence_of(savory)}
-    it {should validate_presence_of(sweet)}
-    it {should validate_presence_of(rating)}
+    it {should validate_presence_of(:name)}
+    it {should validate_presence_of(:description)}
+    it {should validate_presence_of(:savory)}
+    it {should validate_presence_of(:sweet)}
+    it {should validate_presence_of(:rating)}
   end
 end
