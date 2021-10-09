3b7ead670c12966c3ed50efe95ce9531f19cee9a
Caleb1991 <calebjwittman1991@gmail.com>
2021-10-09 16:26:39 -0600

    Add shoulda matchers to gem file and helper, update validations test, create User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User do
   describe '#validations' do
-    it {validates_presence_of(:username)}
-    it {validates_presence_of(:first_name)}
-    it {validates_presence_of(:last_name)}
-    it {validates_presence_of(:email)}
-    it {validates_presence_of(:password)}
+    it {should validate_presence_of(:username)}
+    it {should validate_presence_of(:first_name)}
+    it {should validate_presence_of(:last_name)}
+    it {should validate_presence_of(:email)}
+    it {should validate_presence_of(:password)}
   end
 end
