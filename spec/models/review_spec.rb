4d35a88df47341b58d47014d72368fbc1adb415e
Caleb1991 <calebjwittman1991@gmail.com>
2021-10-10 08:04:38 -0600

    Add validation and relationship tests for reviews

---
+++ spec/models/review_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Review do
+  describe 'validations' do
+    it {should validate_presence_of(:description)}
+    it {should validate_presence_of(:rating)}
+    it {should validate_presence_of(:users_snack_id)}
+  end
+
+  describe 'relationships' do
+    it {should belong_to(:users_snack)}
+    it {should belong_to(:users).through(:users_snack)}
+    it {should belong_to(:snacks).through(:users_snack)}
+  end
+end
