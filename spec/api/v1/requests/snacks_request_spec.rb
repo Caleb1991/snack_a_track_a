3c829185b75fa9f306b34a6e1b564a6d8835605c
Caleb1991 <calebjwittman1991@gmail.com>
2021-10-09 21:07:01 -0600

    Add test for creating a snack

---
+++ spec/api/v1/requests/snacks_request_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe 'Snack API' do
+  before :each do
+    @snack = Snack.create!(name: 'Funyuns', description: 'DELICIOUS', savory: true, sweet: false, rating: 4.5)
+  end
+
+  describe '#create' do
+    it 'creates a snack' do
+      snack_payload = {
+        name: 'Doritos',
+        description: 'BOLD',
+        savory: true,
+        sweet: false,
+        rating: 2.5
+      }
+
+      expect(Snack.all.count).to eq(1)
+
+      post '/api/v1/snacks', params: snack_payload, as: :json
+
+      expect(response).to be_successful
+
+      created_snack = JSON.parse(response.body, symbolize_names: true)
+
+      expect(created_snack[:data][:attributes][:name]).to eq(snack_payload[:name])
+      expect(created_snack[:data][:attributes][:description]).to eq(snack_payload[:description])
+      expect(created_snack[:data][:attributes][:savory]).to eq(snack_payload[:savory])
+      expect(created_snack[:data][:attributes][:sweet]).to eq(snack_payload[:sweet])
+      expect(created_snack[:data][:attributes][:rating]).to eq(snack_payload[:rating])
+    end
+  end
+end
