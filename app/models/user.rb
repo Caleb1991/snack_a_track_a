5ef62bc2ae3ec12fe50161b70abc1e6735888eae
Caleb1991 <calebjwittman1991@gmail.com>
2021-10-10 15:02:32 -0600

    Add tests for users snacks functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   validates_presence_of :first_name, :last_name, :email, :password_digest
   has_many :users_snack, dependent: :destroy
   has_many :snacks, through: :users_snack
-  has_many :reviews, through: :users_snack
+  has_many :reviews, through: :users_snack, dependent: :destroy
   has_secure_password
 end
