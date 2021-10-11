c0c31bb59011317a98958d208f03373d31f04bac
Caleb1991 <calebjwittman1991@gmail.com>
2021-10-11 09:10:28 -0600

    Create testing for average rating of a snack

--- spec/models/snack_spec.rb
+++ spec/models/snack_spec.rb
@@ -1,6 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Snack do
+  before :each do
+    @user_1 = User.create!(username: 'Roald1991', first_name: 'Roald', last_name: 'Roaldington', email: 'RoaldRules91@Gmail.Com', password: 'PenguinsRule11', password_confirmation: 'PenguinsRule11')
+    @user_2 = User.create!(username: 'Larry2020', first_name: 'Larry', last_name: 'Larryington', email: 'LarryRules91@Gmail.Com', password: 'PolarBearsRule11', password_confirmation: 'PolarBearsRule11')
+
+    @snack_1 = Snack.create!(name: 'Funyuns', description: 'Oniony', savory: true, sweet: false)
+
+    @users_snack_1 = UsersSnack.create!(user_id: @user_1.id, snack_id: @snack_1.id)
+    @users_snack_2 = UsersSnack.create!(user_id: @user_2.id, snack_id: @snack_1.id)
+
+    @review_1 = @users_snack_1.reviews.create!(description: 'What a fun way to eat onions, I couldnt believe it when they said they were raw.', rating: 4.6)
+    @review_2 = @users_snack_2.reviews.create!(description: 'TASTY!', rating: 5.0)
+  end
+
   describe 'validations' do
     it {should validate_presence_of(:name)}
     it {should validate_presence_of(:description)}
@@ -10,5 +23,11 @@ RSpec.describe Snack do
     it {should have_many(:users_snack)}
     it {should have_many(:users).through(:users_snack)}
     it {should have_many(:reviews).through(:users_snack)}
+  end
+
+  describe '#average_rating' do
+    it 'returns the average rating based on all reviews for a given snack' do
+      expect(@snack_1.average_rating).to eq(4.8)
+    end
   end
 end
