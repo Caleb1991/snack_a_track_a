3a5dd40d827f3ed5b8b833c9ffd814c58102bcb3
Caleb1991 <calebjwittman1991@gmail.com>
2021-10-11 07:56:06 -0600

    Add user and snack relationship to review

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,6 @@ class Review < ApplicationRecord
   validates_presence_of :description, :users_snack_id
   validates_presence_of :rating, numericality: true
   belongs_to :users_snack
+  has_many :snacks, through: :users_snack
+  has_many :users, through: :users_snack
 end
