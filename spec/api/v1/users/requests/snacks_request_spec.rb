51a2c240efc8ffee47a968e26ee59aab14a5d8b6
Caleb1991 <calebjwittman1991@gmail.com>
2021-10-12 14:32:56 -0600

    Add test for sending a limit parameter to method

--- spec/api/v1/users/requests/snacks_request_spec.rb
+++ spec/api/v1/users/requests/snacks_request_spec.rb
@@ -9,18 +9,24 @@ RSpec.describe 'User Snack API' do
     @snack_3 = Snack.create!(name: 'Honey Bun', description: 'Honey and Bun-y', savory: false, sweet: true)
     @snack_4 = Snack.create!(name: 'Doritos', description: 'Bold', savory: true, sweet: false)
     @snack_5 = Snack.create!(name: 'Cheetos', description: 'The one with the cheetah', savory: true, sweet: false)
+    @snack_6 = Snack.create!(name: 'Nachos', description: 'Not bad', savory: true, sweet: false)
+    @snack_7 = Snack.create!(name: 'Hot Dogs', description: 'Also not impressive', savory: true, sweet: false)
 
     @users_snack_1 = UsersSnack.create!(user_id: @user_1.id, snack_id: @snack_1.id)
     @users_snack_2 = UsersSnack.create!(user_id: @user_1.id, snack_id: @snack_2.id)
     @users_snack_3 = UsersSnack.create!(user_id: @user_1.id, snack_id: @snack_3.id)
     @users_snack_4 = UsersSnack.create!(user_id: @user_1.id, snack_id: @snack_4.id)
     @users_snack_5 = UsersSnack.create!(user_id: @user_1.id, snack_id: @snack_5.id)
+    @users_snack_6 = UsersSnack.create!(user_id: @user_1.id, snack_id: @snack_6.id)
+    @users_snack_7 = UsersSnack.create!(user_id: @user_1.id, snack_id: @snack_7.id)
 
     @review_1 = @users_snack_1.reviews.create!(description: 'What a fun way to eat onions, I couldnt believe it when they said they were raw.', rating: 4.6)
     @review_2 = @users_snack_2.reviews.create!(description: 'Out of this world.', rating: 4.7)
     @review_3 = @users_snack_3.reviews.create!(description: 'The only way I can think to save the bees.', rating: 4.8)
     @review_4 = @users_snack_4.reviews.create!(description: 'Best paired with a Mountain Dew.', rating: 4.9)
     @review_5 = @users_snack_5.reviews.create!(description: 'Again, the one with the cheetah.', rating: 5.0)
+    @review_5 = @users_snack_6.reviews.create!(description: 'Again, the one with the cheetah.', rating: 1.0)
+    @review_5 = @users_snack_7.reviews.create!(description: 'Again, the one with the cheetah.', rating: 0.5)
   end
 
   describe '#index' do
@@ -56,6 +62,18 @@ RSpec.describe 'User Snack API' do
       expect(highest_rated_snacks[:data][:attributes][:snacks].count).to eq(5)
       expect(highest_rated_snacks[:data][:attributes][:snacks][0][:name]).to eq('Cheetos')
       expect(highest_rated_snacks[:data][:attributes][:snacks][4][:name]).to eq('Funyuns')
+    end
+
+    it 'can accept a limit amount' do
+      get "/api/v1/users/#{@user_1.id}/snacks/top_rated_snacks?limit=7"
+
+      expect(response).to be_successful
+
+      highest_rated_snacks = JSON.parse(response.body, symbolize_names: true)
+
+      expect(highest_rated_snacks[:data][:attributes][:snacks].count).to eq(7)
+      expect(highest_rated_snacks[:data][:attributes][:snacks][0][:name]).to eq('Cheetos')
+      expect(highest_rated_snacks[:data][:attributes][:snacks][6][:name]).to eq('Funyuns')
     end
 
     it 'returns an error is user cant be found' do
