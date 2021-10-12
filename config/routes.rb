0e431c1912132ebedbeb89f862023ae525c0b579
Caleb1991 <calebjwittman1991@gmail.com>
2021-10-12 15:23:36 -0600

    Create top rated savory or sweet request

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
       end
       get '/snacks/top_rated_snacks_overall', to: '/api/v1/snacks#top_rated_snacks_overall'
+      get '/snacks/top_rated_savory_or_sweet_snacks', to: '/api/v1/snacks#top_rated_savory_or_sweet_snacks'
       get '/snacks/:snack_id/average_rating', to: '/api/v1/snacks#average_rating'
       resources :snacks
       resources :users_snacks, only: [:create, :destroy]
