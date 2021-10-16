5fc9dc7a9256722eb60354e8306373e324b8f31c
Caleb1991 <calebjwittman1991@gmail.com>
2021-10-16 16:20:12 -0600

    Remove binding.pry

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by(username: user_credentials[:username])
-    binding.pry
+
     if user && user.authenticate(user_credentials[:password])
       render json: UserSerializer.successful_login(user), status: 201
     else
